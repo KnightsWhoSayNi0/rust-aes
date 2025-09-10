c5f7c72557ac8ddd3d2a659473bee9857b518828
KnightsWhoSayNi <johngodman006@gmail.com>
2025-09-10 19:22:33 -0400

    Add function steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Rust AES Implementation
+//!
+//! Trying my best to implement the AES encryption algorithm in Rust
+//! NIST FIPS-197
+//!
+//! John Godman
+
+fn cipher_128(input: &[u8; 16], key: [[u8; 4]; 4]) {
+    // put input into the state
+    let mut state = [[0u8; 4]; 4];
+
+    // add round key
+
+    for round in 0..10 {
+        // sub bytes
+        // shift rows
+        // mix columns
+        // add round key
+    }
+
+    // sub bytes
+    // shift rows
+    // add round key
+
 }
+
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::cipher_128;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn cipher_test() {
+
     }
 }
